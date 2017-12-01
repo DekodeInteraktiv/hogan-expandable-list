723cd8f5a81206b41d4cf9d483578b727100e858
pederan <peder1976@gmail.com>
2017-12-01 16:25:41 +0100

    animation on x when opening and closing list item

--- hogan-expandable-list.php
+++ hogan-expandable-list.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-expandable-list
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/hogan-expandable-list
  * Description: Expandable List Module for Hogan
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0
