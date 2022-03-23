cd0c598a9363e728495a7a5e54cc85594701a6f8
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2022-03-23 15:06:25 +0100

    Version bump

--- hogan-expandable-list.php
+++ hogan-expandable-list.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-expandable-list
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/hogan-expandable-list
  * Description: Expandable List Module for Hogan
- * Version: 1.2.1
+ * Version: 1.2.2
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HOGAN_EXPANDABLE_LIST_VERSION', '1.2.1' );
+define( 'HOGAN_EXPANDABLE_LIST_VERSION', '1.2.2' );
 
 add_action( 'plugins_loaded', __NAMESPACE__ . '\\hogan_load_textdomain' );
 add_action( 'hogan/include_modules', __NAMESPACE__ . '\\register_module', 10, 1 );
